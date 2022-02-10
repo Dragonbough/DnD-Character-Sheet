33d0c13ca776925526779bde300bbc8d657eb25d
Dragonbough <99271006+Dragonbough@users.noreply.github.com>
2022-02-10 07:53:18 +0000

    Added the rest of the Classes

--- D&D Character Sheet.py
+++ D&D Character Sheet.py
@@ -14,8 +14,47 @@ def cclass():
     print ("Fighter?")
     c_fighter=input("Y or N?")
     if c_fighter == "n" or " n":
-        print ("Barbarian")
+        print ("Barbarian?")
         c_barbarian=input("Y or N?")
+        if c_barbarian == "n" or " n":
+            print ("Bard?")
+            c_bard=input("Y or N?")
+            if c_bard == "n" or " n":
+                print ("Cleric?")
+                c_cleric=input("Y or N?")
+                if c_cleric == "n" or " n":
+                    print ("Druid?")
+                    c_druid=input("Y or N?")
+                    if c_druid == "n" or " n":
+                        print ("Monk?")
+                        c_monk=input("Y or N?")
+                        if c_monk == "n" or " n":
+                            print ("Paladin?")
+                            c_paladin=input("Y or N?")
+                            if c_paladin == "n" or " n":
+                                print ("Ranger?")
+                                c_ranger=input("Y or N?")
+                                if c_ranger == "n" or " n":
+                                    print ("Rogue?")
+                                    c_rogue=input("Y or N?")
+                                    if c_rogue == "n" or " n":
+                                        print ("Sorcerer?")
+                                        c_sorcerer=input("Y or N?")
+                                        if c_sorcerer == "n" or " n":
+                                            print ("Warlock")
+                                            c_warlock=input("Y or N?")
+                                            if c_warlock == "n" or " n":
+                                                print ("Wizard?")
+                                                c_wizard=input("Y or N?")
+                                                if c_wizard == "n" or " n":
+                                                    ()cclass
+
+                                  
+
+                
+                                 
+    
+    
     
         
     
