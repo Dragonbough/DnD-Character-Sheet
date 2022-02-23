d6cc39b02b34bca60a5f6249013852d447b35750
Dragonbough <99271006+Dragonbough@users.noreply.github.com>
2022-02-23 12:18:10 +0000

    Refactored class code
    The bug should be fixed. Hopefully. Maybe. Probably not.

--- D&D Character Sheet.py
+++ D&D Character Sheet.py
@@ -136,11 +136,11 @@ def race():
         c_race = "Half-Elf"
         bground()
     if r_h_elf == "n":
-       print ("Halfling?")
-       r_halfling = input ("Y or N?")
+        print ("Halfling?")
+        r_halfling = input ("Y or N?")
     if r_halfling == "y":
-       c_race = "Halfling"
-       bground()
+        c_race = "Halfling"
+        bground()
     if r_halfling == "n":
         print ("Half-Orc")
         r_h_orc = input ("Y or N?")
@@ -161,77 +161,77 @@ def cclass():
     print("What is your character's class?")
     print ("Fighter?")
     c_fighter = input("Y or N?")
-    if c_fighter == "n":
-        print ("Barbarian?")
-        c_barbarian = input("Y or N?")
-        if c_barbarian == "n":
-            print ("Bard?")
-            c_bard = input("Y or N?")
-            if c_bard == "n":
-                print ("Cleric?")
-                c_cleric = input("Y or N?")
-                if c_cleric == "n":
-                    print ("Druid?")
-                    c_druid = input("Y or N?")
-                    if c_druid == "n":
-                        print ("Monk?")
-                        c_monk = input("Y or N?")
-                        if c_monk == "n":
-                            print ("Paladin?")
-                            c_paladin = input("Y or N?")
-                            if c_paladin == "n":
-                                print ("Ranger?")
-                                c_ranger = input("Y or N?")
-                                if c_ranger == "n":
-                                    print ("Rogue?")
-                                    c_rogue = input("Y or N?")
-                                    if c_rogue == "n":
-                                        print ("Sorcerer?")
-                                        c_sorcerer = input("Y or N?")
-                                        if c_sorcerer == "n":
-                                            print ("Warlock")
-                                            c_warlock = input("Y or N?")
-                                            if c_warlock == "n":
-                                                print ("Wizard?")
-                                                c_wizard = input("Y or N?")
-                                                if c_wizard == "n":
-                                                    cclass()
     if c_fighter == "y":
         c_class = "Fighter"
         race()
+    if c_fighter == "n":
+        print ("Barbarian?")
+        c_barbarian = input("Y or N?")
     if c_barbarian == "y":
         c_class = "Barbarian"
         race()
+    if c_barbarian == "n":
+        print ("Bard?")
+        c_bard = input("Y or N?")
     if c_bard == "y":
         c_class = "Bard"
         race()
+    if c_bard == "n":
+        print ("Cleric?")
+        c_cleric = input("Y or N?")
     if c_cleric == "y":
         c_class = "Cleric"
         race()
+    if c_cleric == "n":
+        print ("Druid?")
+        c_druid = input("Y or N?")
     if c_druid == "y":
         c_class = "Druid"
         race()
+    if c_druid == "n":
+        print ("Monk?")
+        c_monk = input("Y or N?")
     if c_monk == "y":
         c_class = "Monk"
         race()
+    if c_monk == "n":
+        print ("Paladin?")
+        c_paladin = input("Y or N?")
     if c_paladin == "y":
         c_class = "Paladin"
         race()
+    if c_paladin == "n":
+        print ("Ranger?")
+        c_ranger = input("Y or N?")
     if c_ranger == "y":
         c_class = "Ranger"
         race()
+    if c_ranger == "n":
+        print ("Rogue?")
+        c_rogue = input("Y or N?")
     if c_rogue == "y":
         c_class = "Rogue"
         race()
+    if c_rogue == "n":
+        print ("Sorcerer?")
+        c_sorcerer = input("Y or N?")
     if c_sorcerer == "y":
         c_class = "Sorcerer"
         race()
+    if c_sorcerer == "n":
+        print ("Warlock")
+        c_warlock = input("Y or N?")
     if c_warlock == "y":
         c_class = "Warlock"
         race()
+    if c_warlock == "n":
+        print ("Wizard?")
+        c_wizard = input("Y or N?")
     if c_wizard == "y":
         c_class = "Wizard"
         race()
+    if c_wizard == "n":
+        cclass()
 
 def name():
     ch_name=input("What is your character's name?")
